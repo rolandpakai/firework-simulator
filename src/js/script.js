b8fb2f06da37f6a9eab4e6f0e09bfd29b359cab2
Roland Pákai <roland.pakai@liferay.com>
2019-10-11 15:16:23 +0200

    Update soundManager baseURL

--- src/js/script.js
+++ src/js/script.js
@@ -2098,7 +2098,7 @@ const Spark = {
 
 
 const soundManager = {
-	baseURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/329180/',
+	baseURL: './mp3/',
 	ctx: new (window.AudioContext || window.webkitAudioContext),
 	sources: {
 		lift: {
